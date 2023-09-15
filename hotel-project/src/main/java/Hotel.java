8820044d31919599e4160bb0e1e9dfe6851ba572
thomaschor <99774559+thomaschor@users.noreply.github.com>
2023-09-14 21:35:22 -0700

    Removed excess package folders "com, company", etc

--- hotel-project/src/main/java/Hotel.java
+++ hotel-project/src/main/java/Hotel.java
@@ -1,10 +1,10 @@
 
-package com.mycompany.hotel.project;
 
 /**
  *
  * @author Thomas, Keaton, Jonathan
  */
+
 public class Hotel {
    public static void main(String[] args) {
         System.out.println("The mitochondria is the powerhouse of the cell");
