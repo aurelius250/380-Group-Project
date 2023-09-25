35873b245d0bfe0dee57981a826cdcf85a242f46
thomaschor <99774559+thomaschor@users.noreply.github.com>
2023-09-25 15:34:52 -0700

    Added test files and test package

---
+++ Hotel Prototype/src/TestPackage/TestSuite.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package TestPackage;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ *
+ * @author tomc
+ */
+public class TestSuite {
+    @Test
+    void testAddition() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+}
