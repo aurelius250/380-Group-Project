6c297c13db9d3b74b3be9272fade42d17e89ac7a
krooton <104192072+krooton@users.noreply.github.com>
2023-09-25 22:16:31 -0700

    Adding print message to "Book Reservation" button

--- Hotel Prototype/src/hotel/prototype/MainController.java
+++ Hotel Prototype/src/hotel/prototype/MainController.java
@@ -5,8 +5,12 @@
 package hotel.prototype;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 
 /**
  * FXML Controller class
@@ -15,9 +19,20 @@ import javafx.fxml.Initializable;
  */
 public class MainController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    
+    private DatePicker fromDatePicker,toDatePicker;
+    private LocalDate fromDate, toDate;
+    
+    public void bookReservation(ActionEvent e){
+        fromDate = fromDatePicker.getValue();
+        toDate = toDatePicker.getValue();
+        
+        System.out.println("Reservation confirmed from " + fromDate + 
+                " to " + toDate + ".");
+    }
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
