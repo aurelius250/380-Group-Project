303b3fb25d8355f062045abc1e936873d89ee821
Jonathan Johnson <jon01mtnz@hotmail.com>
2023-09-27 15:56:22 -0700

    Made a basic room class
    Including basic constructors

---
+++ Hotel Prototype/src/hotel/prototype/Room.java
@@ -0,0 +1,19 @@
+package hotel.prototype;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author jon01
+ */
+public class Room {
+    public Room(){
+        boolean smoking = false;
+        int numBeds = 1;
+        int numPeople = 1;
+        
+    }
+}
