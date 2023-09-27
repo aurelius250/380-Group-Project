3fcea5409705aca6fdcfbca9bf5deabb90015e6f
Jonathan Johnson <jon01mtnz@hotmail.com>
2023-09-27 16:00:02 -0700

    More Room info and constructors

--- Hotel Prototype/src/hotel/prototype/Room.java
+++ Hotel Prototype/src/hotel/prototype/Room.java
@@ -4,16 +4,36 @@ package hotel.prototype;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author jon01
  */
 public class Room {
-    public Room(){
-        boolean smoking = false;
-        int numBeds = 1;
-        int numPeople = 1;
-        
+    private int roomNum;
+    private boolean smoking;
+    private int numBeds;
+    private int numPeople;
+    private int sqftSize;
+    private char bedType;
+    private String desc;
+
+    public Room() {
+        roomNum = 0;
+        smoking = false;
+        numBeds = 1;
+        numPeople = 1;
+        sqftSize = 200;
+        bedType = 'Q';
+        desc = "Basic room";
+    }
+
+    public Room(int i,boolean a, int b, int c, int d, char e, String f) {
+        roomNum = i;
+        smoking = a;
+        numBeds = b;
+        numPeople = c;
+        sqftSize = d;
+        bedType = e;
+        desc = f;
     }
 }
