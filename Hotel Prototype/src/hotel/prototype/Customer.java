947eaa56ef8853984cff78511ace03e9427b0cc4
thomaschor <99774559+thomaschor@users.noreply.github.com>
2023-10-09 13:41:54 -0700

    Added toString for the 3 base variables in class

--- Hotel Prototype/src/hotel/prototype/Customer.java
+++ Hotel Prototype/src/hotel/prototype/Customer.java
@@ -4,10 +4,60 @@
  */
 package hotel.prototype;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author tomc
  */
 public class Customer {
+    protected String customerName; 
+    protected String customerEmail; 
+    protected ArrayList reservationIDs; 
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Customer Name: ").append(customerName).append("\n");
+        sb.append("Customer Email: ").append(customerEmail).append("\n");
+        sb.append("Reservation IDs: ").append(reservationIDs).append("\n");
+        return sb.toString();
+    }
+    
+    protected Customer() { 
+        this.customerName = ""; 
+        this.customerEmail = ""; 
+        this.reservationIDs = new ArrayList<>();
+    } 
+
+    protected Customer(String customerName, String customerEmail, ArrayList<Integer> reservationIDs) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.reservationIDs = reservationIDs;
+    }    
+    
+    protected String getCustomerName() {
+        return customerName;
+    }
+
+    protected void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    protected String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    protected void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    protected ArrayList<Integer> getReservationIDs() {
+        return reservationIDs;
+    }
+
+    protected void setReservationIDs(ArrayList<Integer> reservationIDs) {
+        this.reservationIDs = reservationIDs;
+    }
     
 }
