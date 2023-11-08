cc7ee3afa7e099299382c208ecea519b48d1cf05
Jonathan Johnson <jon01mtnz@hotmail.com>
2023-11-07 17:28:17 -0800

    Formatting and code for replaceLine
    replaceLine method should do what the name implies now. It will read all the data in the file to a string and then replace the designated line with the new data before writing over the file.

--- Hotel Prototype/src/hotel/prototype/FileController.java
+++ Hotel Prototype/src/hotel/prototype/FileController.java
@@ -19,37 +19,41 @@ import javafx.beans.property.SimpleIntegerProperty;
  * @author Jonathan
  */
 public class FileController {
+
     //private static String roomsFile = "rooms.txt";
     /**
      * Reads from a text file and generates an ArrayList of strings of the whole
      * unbroken line from the text file
+     *
      * @param fileName name of the text file to be read from
      * @return An ArrayList of room objects
      */
-    public static ArrayList readFile(String fileName){
+    public static ArrayList readFile(String fileName) {
         String fileData;
         String[] dataArr = null;
         ArrayList<String> list = new ArrayList<String>();
-        
-        try{
+
+        try {
             FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
-            
-            while ((fileData = br.readLine()) != null){
+
+            while ((fileData = br.readLine()) != null) {
                 list.add(fileData);
             }
-            
+
             br.close();
             fr.close();
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-        
+
         return list;
     }
-    
+
     /**
-     * Reads from a text file and generates an ArrayList of room objects from the file
+     * Reads from a text file and generates an ArrayList of room objects from
+     * the file
+     *
      * @param fileName name of the text file to be read from
      * @return An ArrayList of room objects
      */
@@ -57,15 +61,15 @@ public class FileController {
         String fileData;
         String[] dataArr = null;
         ArrayList<Room> roomList = new ArrayList<Room>();
-        int roomNum,numBeds,numPeople,sqftSize;
+        int roomNum, numBeds, numPeople, sqftSize;
         boolean smoking;
         char bedType;
         String description;
-        
+
         try {
             FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
-            
+
             while ((fileData = br.readLine()) != null) {
                 dataArr = fileData.split(",");
                 roomNum = integerParser(dataArr[0]);
@@ -75,7 +79,7 @@ public class FileController {
                 sqftSize = integerParser(dataArr[4]);
                 bedType = dataArr[5].charAt(0);
                 description = dataArr[6];
-                        
+
                 roomList.add(new Room(roomNum,
                         smoking,
                         numBeds,
@@ -85,74 +89,77 @@ public class FileController {
                         description));
 
             }
-            
+
             br.close();
             fr.close();
 
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-        
+
         return roomList;
     }
-    
+
     /**
-     * Reads from a file and generates an ArrayList of Customer objects from the file
+     * Reads from a file and generates an ArrayList of Customer objects from the
+     * file
+     *
      * @param fileName name of the text file to be read from
      * @return an ArrayList of Customer objects
      */
     public static ArrayList readCustomerFile(String fileName) {
-        String customerName,customerEmail;
+        String customerName, customerEmail;
         String fileData;
         String[] dataArr = null;
         ArrayList<Customer> customerList = new ArrayList<Customer>();
         ArrayList<Integer> reservationIDs = new ArrayList<Integer>();
-        
+
         try {
             FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
-            
+
             while ((fileData = br.readLine()) != null) {
                 dataArr = fileData.split(",");
                 customerName = dataArr[0];
-                customerEmail= dataArr[1];
-                
-                for(int i = 2; i<dataArr.length;i++){
+                customerEmail = dataArr[1];
+
+                for (int i = 2; i < dataArr.length; i++) {
                     reservationIDs.add(integerParser(dataArr[i]));
                 }
-                
-                customerList.add(new Customer(customerName,customerEmail,reservationIDs));
+
+                customerList.add(new Customer(customerName, customerEmail, reservationIDs));
             }
-            
+
             br.close();
             fr.close();
 
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-        
+
         return customerList;
     }
-    
+
     /**
      * Reads from a file and generates an ArrayList of Reservation objects from
      * the file
+     *
      * @param fileName name of the text file to be read from
      * @return an ArrayList of Reservation objects
      */
     public static ArrayList readReservationFile(String fileName) {
-        String fileData,reservationID;
+        String fileData, reservationID;
         LocalDate checkIn, checkOut;
         String[] dataArr = null;
         ArrayList<Reservation> reservationList = new ArrayList<Reservation>();
-        
+
         try {
             FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
-            
+
             while ((fileData = br.readLine()) != null) {
                 dataArr = fileData.split(",");
-                
+
                 reservationID = dataArr[0];
                 checkIn = dateParser(dataArr[1]);
                 checkOut = dateParser(dataArr[2]);
@@ -165,22 +172,23 @@ public class FileController {
                 ));
 
             }
-            
+
             br.close();
             fr.close();
-            
+
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-        
+
         return reservationList;
     }
 
-    
-    
     /**
      * Method for deleting a line from a text file
-     * @param idOrNum the reservation id or room number you are looking to have removed from the file
+     *
+     * @param fileName name of the text file to be altered
+     * @param idOrNum the reservation id or room number you are looking to have
+     * removed from the file
      */
     public static void removeLine(String fileName, int idOrNum) {
         try {
@@ -190,16 +198,16 @@ public class FileController {
             BufferedWriter bw = new BufferedWriter(new FileWriter(temp));
             String[] dataArr;
             String currentLine;
-            while((currentLine = br.readLine())!= null){
+            while ((currentLine = br.readLine()) != null) {
                 dataArr = currentLine.split(",");
-                if(integerParser(dataArr[0])==idOrNum){
+                if (integerParser(dataArr[0]) == idOrNum) {
                     currentLine = "";
                 }
                 bw.write(currentLine + System.getProperty("line.seperator"));
             }
             bw.close();
             br.close();
-            
+
             File oldFile = new File(fileName);
             temp.renameTo(oldFile);
             oldFile.delete();
@@ -207,17 +215,43 @@ public class FileController {
             ioe.printStackTrace();
         }
     }
-    
-    public static void replaceLine(String filename, String newLine, String oldLine){
-    
-    }
-    
     /**
-    * Method for appending string data to lines in a text file
-    * @param fileName The name of the file to have text added to
-    * @param data The string data of the text to be added to the file
-    * @throws IOException 
-    */
+     * Line replacement method for modifying old text file data
+     * 
+     * @param filename name of the text file to be altered
+     * @param newLine new data to replace old in text file
+     * @param oldLine old data to be replaced in text file
+     */
+    public static void replaceLine(String filename, String newLine, String oldLine) {
+        try {
+            File file = new File(filename);
+            FileWriter fr = new FileWriter(file);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String oldData = "";
+            String newData;
+            String currentLine;
+            
+            while ((currentLine = br.readLine()) != null){
+                oldData = oldData + currentLine + System.lineSeparator();
+            }
+            
+            newData = oldData.replaceAll(oldLine, newLine);
+            fr.write(newData);
+            br.close();
+            fr.close();
+            
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    /**
+     * Method for appending string data to lines in a text file
+     * 
+     * @param fileName The name of the file to have text added to
+     * @param data The string data of the text to be added to the file
+     * @throws IOException
+     */
     public static void appendFile(String fileName, String data) throws IOException {
         try (FileWriter fw = new FileWriter(fileName, true)) {
             fw.append(data);
@@ -226,9 +260,10 @@ public class FileController {
             ioe.printStackTrace();
         }
     }
-    
+
     /**
      * Method for converting string data to integer data
+     *
      * @param stringData The string to be converted to an integer
      * @return The integer value of the converted string
      */
@@ -239,9 +274,10 @@ public class FileController {
             return 0;
         }
     }
-    
+
     /**
      * Method for converting string data to Boolean data
+     *
      * @param stringData The string to be converted to an integer
      * @return The Boolean value of the converted string
      */
@@ -252,9 +288,10 @@ public class FileController {
             return false;
         }
     }
-    
+
     /**
      * Method for converting string data to data of the LocalDate type
+     *
      * @param stringData The string to be converted to the LocalDate type
      * @return The LocalDate equivalent of the converted string
      */
